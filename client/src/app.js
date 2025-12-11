a6b830a710be24968be4df8ad2c4e11a8f8814f3
arronburgess <generalkajgue@hotmail.com>
2025-12-11 16:17:05 +0000

    chore: fixed up some of the initial setup

--- client/src/app.js
+++ client/src/app.js
@@ -4,13 +4,13 @@
 
 //fetch the POST server route
 
-fetch("url", {
-    method:,
-    headers: {
+// fetch("url", {
+//     method:,
+//     headers: {
 
-    },
-    body: JSON.stringify()
-})
+//     },
+//     body: JSON.stringify()
+// })
 
 //! Once you finish your project, replace your localhost url with the deployed server URL from Render!!!
 //=================================
@@ -20,3 +20,25 @@ fetch("url", {
 //fetch the GET route from the server
 
 // Render the data using DOM elements like normal! (One per piece of data)
+
+const guestbookForm = document.getElementById("guest-book-form");
+console.log(guestBookForm);
+
+function handleGuestSubmit(event) {
+  event.preventDefault();
+  const formDataTemplate = new FormData(guestbookForm);
+  const formValues = Object.fromEntries(formDataTemplate);
+  console.log(formValues);
+  //TODO: send the data in a request to the server
+  // We'll use the URL where our server is located! http://localhost:8080/new-staff temporarily (make sure that you are fetching the specific POST route that you are requesting data)
+  // We'll use fetch to connect our client <---> server!
+  fetch("http://localhost:8080/new-staff", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ formValues }),
+  });
+}
+
+guestbookForm.addEventListener("submit", handleGuestSubmit);
--- server/dbConnection.js
+++ server/dbConnection.js
@@ -1,1 +1,15 @@
 // TODO set up a pool to connect to our DB Like the other day!
+
+// dbConnection.js file
+//imports
+
+import pg from "pg";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+export const db = new pg.Pool({
+  connectionString: process.env.DATABASE_URL,
+});
+
+//================================
